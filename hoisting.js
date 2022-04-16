421b0c7b1218373ee0de59c69f9a1ec8fe79a883
indhiranis <99375735+indhiranis@users.noreply.github.com>
2022-04-16 23:58:57 +0530

    hoisting in js

---
+++ hoisting.js
@@ -0,0 +1,10 @@
+addNoDeclaration();
+function addNoDeclaration(){
+    console.log("statement 1");
+}
+
+//another function call
+function addNoDeclaration(){
+    console.log("statement 1");
+}
+addNoDeclaration();
